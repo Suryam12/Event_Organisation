8588033f54e9dcb5d3a7e2f86aa81a1f8a685832
SURYAM <suryam420024@gmail.com>
2025-01-03 14:57:07 +0530

    initial commits

---
+++ ignition/modules/EventContract.js
@@ -0,0 +1,14 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const TokenModule = buildModule("TokenModule", (m) => {
+  const token = m.contract("EventContract");
+
+  return { token };
+});
+
+module.exports = TokenModule;
+
+
+
+
+
